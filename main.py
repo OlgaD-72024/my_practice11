fcf8029f96b322de8ee4b5b6efef91ee03a1a57d
OlgaD-72024 <yachhik@gmail.com>
2024-08-26 21:44:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def get_matrix(n, m, value):
         for j in range(m):
             row.append(value)
         matrix.append(row)
-    return matrix, row
+    return matrix
 
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
